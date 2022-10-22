2eb1f40b40ba72ebcc5710c7f6ed71aad3ccaa64
Elias Lolck <elias@lolck.com>
2022-10-22 03:57:00 +0200

    fastmod capable of big negative numbers

--- various/fastmod.h
+++ various/fastmod.h
@@ -10,14 +10,16 @@ template <int MOD> struct FastMod{
     ll reduce(long long a) { 
         if(a < MOD && a >= 0)return a;
         a = a - static_cast <unsigned long long> ((__uint128_t(m) * a) >> 64) * MOD;
-        if(a >= MOD) a -= MOD;
+	a += MOD * (a < 0);
+	a -= MOD * (a >= MOD);
         return a;
         //return a % b;
     }
     ll reducephi(long long a) { 
         if(a < MODPHI && a >= 0)return a;
         a = a - static_cast <unsigned long long> ((__uint128_t(mphi) * a) >> 64) * MODPHI;
-        if(a >= MODPHI) a -= MODPHI;
+	a += MODPHI * (a < 0);
+	a -= MODPHI * (a >= MODPHI);
         return a;
         //return a % b;
     }

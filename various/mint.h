5adbf0e9d77dd4888bd14ac6d1f40e5ecec5aa65
Elias Lolck <elias@lolck.com>
2022-10-22 02:29:03 +0200

    updated mint

--- various/mint.h
+++ various/mint.h
@@ -15,21 +15,21 @@ template <typename A> A inverse(A a, A b) {
 		return b == 1 ? 0 : -1;
 	}
 	int c = inverse(b,a);
-	return c == -1 ? -1 : ((1LL - (long long)b * (long long)c) / (long long)a) % b;
+	return c == -1 ? -1 : ((1LL - static_cast<long long>(b) * static_cast<long long>(c)) / static_cast<long long>(a)) % b;
 }
 
 struct Mint {
 	long long val;
 	explicit operator int() const { return val; }
 	Mint(long long _val = 0) : val(mod.reduce(_val)) {}
-	template <typename B = int> Mint(const B& _val) : val(mod.reduce((long long)_val)) {}
+	template <typename B = int> Mint(const B& _val) : val(mod.reduce(static_cast<long long>(_val))) {}
 	friend bool operator == (const Mint &a, const Mint &b) { return a.val == b.val; }
 	friend bool operator != (const Mint &a, const Mint &b) { return !(a == b); }
 	friend bool operator < (const Mint &a, const Mint &b) { return a.val < b.val; }
 	Mint operator -() const { return Mint(-val); }
 	Mint &operator += (const Mint &o) {
 		val += o.val;
-		if(val >= (int)mod.b)val -= mod.b;
+		if(val >= static_cast<int>(mod.b))val -= mod.b;
 		return *this;
 	}
 	Mint &operator -= (const Mint &o) {
@@ -38,7 +38,7 @@ struct Mint {
 		return *this;
 	}
 	Mint &operator *= (const Mint &o) {
-		val = mod.reduce((long long)val * (long long)o.val);
+		val = mod.reduce(static_cast<long long>(val) * static_cast<long long>(o.val));
 		return *this;
 	}
 	friend Mint pow(Mint a, long long p) {
@@ -53,7 +53,7 @@ struct Mint {
 	}
 	friend Mint inv(const Mint& a) {
 		if constexpr(pri_mod) return pow(a, mod.b - 2);
-		return inverse(a.val, (int)mod.b);
+		return inverse(static_cast<int>(a.val), static_cast<int>(mod.b));
 	}
 	Mint &operator /= (const Mint &o) {
 		(*this) *= inv(o);
@@ -80,7 +80,7 @@ struct Mint {
 	friend istream& operator >> (istream& s, Mint& a) {
 		long long value;
 		s >> value;
-		a.val = mod.reuce(value);
+		a.val = mod.reduce(value);
 		return s;
 	}
 	friend ostream& operator << (ostream& s, const Mint& a) {
